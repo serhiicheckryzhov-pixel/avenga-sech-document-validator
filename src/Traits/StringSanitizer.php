ae34ef9b6384bc91809127cfc9b26b33c1e1ac4c
SECH <sc@belisar.de>
2026-07-02 13:27:11 +0300

    Unnecessary code removed

--- src/Traits/StringSanitizer.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace src\Traits;
-
-Trait StringSanitizer {
-    protected function sanitize(string $content) : string {
-        return strtolower(trim(strip_tags($content)));
-    }
-}
